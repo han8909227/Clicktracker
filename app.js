85e1950546d6344ef6469bc2c6db1c696bdd0d98
han8909227 <hbao2016@hotmail.com>
2017-07-24 16:50:37 -0700

    added user stories and working version without table printing

--- app.js
+++ app.js
@@ -1,38 +1,124 @@
 'use strict';
 
-Product.imgEl = document.getElementById('prod1');
-// Product.pic2 = document.getElementById('pic2');
-// Product.pic3 = document.getElementById('pic3');
+Product.imgOne = document.getElementById('prodOne');
+Product.imgTwo = document.getElementById('prodTwo');
+Product.imgThree = document.getElementById('prodThree');
 
 
 
 Product.allNames = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 Product.all = [];
+Product.click = 0;
+Product.current = [];
+Product.previous = [];
 
 
 function Product(name){
   this.name = name;
   this.source = 'image/' + this.name + '.jpg';
+  this.timesShown = 0;
   Product.all.push(this);
 }
-
-
 
 
 for(var i = 0;i < Product.allNames.length; i++){
   new Product(Product.allNames[i]);
 }
 
-// new Product('banana');
+
 
 
 function randomProd(){
   var randomIndex = Math.floor(Math.random() * Product.allNames.length);
-  Product.imgEl.src = Product.all[randomIndex].source;
-  Product.imgEl.alt = Product.all[randomIndex].name;
-  Product.all[randomIndex].timesShown += 1;
+  // Product.all[randomIndex].timesShown += 1;
+  return Product.all[randomIndex];
 
 }
 
-document.getElementById('prod1').addEventListener('click',randomProd);
-randomProd();
+
+document.getElementById('prodOne').addEventListener('click',handleClick);
+document.getElementById('prodTwo').addEventListener('click',handleClick);
+document.getElementById('prodThree').addEventListener('click',handleClick);
+
+
+function handleClick(e){
+  Product.current = [];
+  Product.click++;
+  for(var i = 0; i < Product.all.length; i++){
+    if(e.target.alt === Product.all[i].name){
+      Product.all[i].timesShown++;
+    }
+  }
+  runSurvey();
+}
+
+
+function testObj(obj,a) {
+  var i = a.length;
+  while (i--) {
+    if (a[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+function runSurvey(){
+  if(Product.click < 25){
+    runOne();
+    runSecond();
+    runThird();
+    Product.previous = Product.current;
+
+  } else{
+    document.getElementById('prodOne').removeEventListener('click',randomProd);
+    document.getElementById('prodTwo').removeEventListener('click',randomProd);
+    document.getElementById('prodThree').removeEventListener('click',randomProd);
+    console.log(Product.click);
+  }
+}
+
+
+
+
+
+function runOne(){
+  var first = randomProd();
+  if(!testObj(first,Product.previous)){
+    Product.imgOne.src = first.source;
+    Product.imgOne.alt = first.name;
+    Product.current.push(first);
+  }
+}
+
+function runSecond(){
+  var second = randomProd();
+  if(!testObj(second,Product.current) && !testObj(second,Product.previous)){
+    Product.imgTwo.src = second.source;
+    Product.imgTwo.alt = second.name;
+    Product.current.push(second);
+  } else{
+    runSecond();
+  }
+}
+
+function runThird(){
+  var third = randomProd();
+  if(!testObj(third,Product.current) && !testObj(third,Product.previous)){
+    Product.imgThree.src = third.source;
+    Product.imgThree.alt = third.name;
+    Product.current.push(third);
+  } else{
+    runThird();
+  }
+}
+
+
+
+
+
+
+
+runSurvey();
---
+++ user_stories.md
@@ -0,0 +1,28 @@
+
+#User stories
+
+<h2> As the focus group participant </h2>
+<ul>
+<li> I want to be able to see the images clearly without any distortion </li>
+<li> I want to be able to click on stuff easily </li>
+<li> I would like the web page to look neat and organized </li>
+<li> I want to be rewarded for any contribution I made </li>
+</ul>
+
+
+<h2> As the market research team member </h2>
+<ul>
+<li> I want the data I collect to be accurate and without bias </li>
+<li> Therefore I want the web page to be easy to read </li>
+<li> I do not want the participant to see the same choices on a page</li>
+<li> I want to be able to access the survey result at the end </li>
+<li> I want the questions/images to be presented in an unordered fasion (random) </li>
+</ul>
+
+<h2> As the developer </h2>
+<ul>
+<li> I want to make sure my codes are organized and functions are clearly separated </li>
+<li> I need to store all the questions/images for the survey in my data base first </li>
+<li> I need to create a random method to select presenting questions </li>
+<li> I need to store user's input somewhere and present/utlize them in the end </li>
+</ul>
